63294f5302ce894dad792db1160bad7de1d6c8b1
Bart Van Hoey <bartvanhoey@hotmail.com>
2019-11-23 16:29:31 +0100

    tutorial 7

--- src/app/todoitems/shared/todo-items.service.ts
+++ src/app/todoitems/shared/todo-items.service.ts
@@ -8,7 +8,7 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class TodoItemsService {
-  private readonly baseUrl = 'https://localhost:5001/api/todoitems';
+  private readonly baseUrl = 'http://localhost:5000/api/todoitems';
 
   constructor(private http: HttpClient) { }
 
